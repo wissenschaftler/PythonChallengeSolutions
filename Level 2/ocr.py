2e2438b97785e0505a1787ce36b0e656b9c7ce29
Wei Xu <wissenschaftler@users.noreply.github.com>
2018-04-25 17:14:42 +0800

    Create ocr.py

---
+++ Level 2/ocr.py
@@ -0,0 +1,7 @@
+strFile = open("ocr.txt",'r') #ocr.txt is where the data is
+wholeStr = strFile.read() 
+fnlStr = []
+for i in wholeStr:
+    if (65 <= ord(i) <= 90) or (97 <= ord(i) <= 122):
+         fnlStr.append(i)
+print ''.join(fnlStr)
